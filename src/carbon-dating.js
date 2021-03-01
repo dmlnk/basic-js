c764633893834391e88fe3b7413006944c9437cf
Daniel Miller <daniel.miller.prs@gmail.com>
2021-03-01 11:11:57 +0100

    fix: some tests were failing

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity != String || Number(sampleActivity) == NaN || sampleActivity === undefined) return false;
+  if (typeof sampleActivity != "string" || isNaN(sampleActivity)|| sampleActivity > 15 || sampleActivity <= 0) return false;
 
   return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 };
